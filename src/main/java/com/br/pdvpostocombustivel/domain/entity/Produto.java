e707c90fab1672b3eb1b8b3c55c3fff752ed62f5
Duust342 <sidneyemanuel01@gmail.com>
2025-09-12 21:03:13 -0300

    Primeira Edição

---
+++ src/main/java/com/br/pdvpostocombustivel/domain/entity/Produto.java
@@ -0,0 +1,62 @@
+package com.br.pdvpostocombustivel.domain.entity;
+
+public class Produto {
+
+    // atributos
+    private String nome;
+    private String referencia;
+    private String fornecedor;
+    private String categoria;
+    private String marca;
+
+    // construtor
+    public Produto(String nome, String referencia, String fornecedor, String categoria, String marca) {
+        this.nome = nome;
+        this.referencia = referencia;
+        this.fornecedor = fornecedor;
+        this.categoria = categoria;
+        this.marca = marca;
+    }
+
+    // getters
+    public String getNome() {
+        return nome;
+    }
+
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public String getFornecedor() {
+        return fornecedor;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    // setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
+
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+}
